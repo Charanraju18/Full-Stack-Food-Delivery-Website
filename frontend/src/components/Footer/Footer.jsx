2ad6b29d5a0ed83c29da634831f9b50facba42ef
Charanraju18 <charanraju925@gmail.com>
2024-12-14 22:22:59 +0530

    completed home page along with responsiveness

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 © TOmato.com - All Rights Reserved
+        Copyright 2024 © Tomato.com - All Rights Reserved
       </p>
     </div>
   );
